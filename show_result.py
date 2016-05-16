4979c193552259cdccb8755accf7079d189fa6cf
fmxustc <fmxustc@mail.ustc.edu.cn>
2016-05-15 18:00:24 -0700

    v1.4

--- show_result.py
+++ show_result.py
@@ -3,7 +3,6 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import warnings
-import matplotlib as mlt
 
 
 def normalize(a):
@@ -18,18 +17,21 @@ def do_nothing(a):
 def draw(d, item, ax, label, func=do_nothing, model='kde'):
     x = item+'1'
     y = item+'2'
-    a = func(d[x].values)
-    b = func(d[y].values)
+    a = np.sort(func(d[x].values))
+    b = np.sort(func(d[y].values))
     mn, mx = max(a.min(), b.min()), min(a.max(), b.max())
     bins_hist = np.linspace(mn, mx, 20)
     bins_kde = np.linspace(mn, mx, 100)
-    ax.set(xlabel=name[item], ylabel='Gaussian Kernel Density')
     if model == 'hist':
+        ax.set(xlabel=name[item], ylabel='Gaussian Kernel Density')
+
         sns.distplot(a, bins=bins_hist, ax=ax, kde=False, hist=True,
                      hist_kws={"histtype": "step", "linewidth": 2, "alpha": 1, "color": 'b', "label": label+'1'})
         sns.distplot(b, bins=bins_hist, ax=ax, kde=False, hist=True,
                      hist_kws={"histtype": "step", "linewidth": 2, "alpha": 1, "color": 'r', "label": label+'2'})
     else:
+        ax.set(xlabel=name[item], ylabel='Count')
+        print(bins_kde)
         sns.distplot(a, bins=bins_kde, ax=ax, label=label+'1', color='b', kde=True, hist=False)
         sns.distplot(b, bins=bins_kde, ax=ax, label=label+'2', color='r', kde=True, hist=False)
 
@@ -45,12 +47,12 @@ fig.suptitle('R Band Differences')
 
 name = {
     'G': 'Gini Index',
-    'M': 'Moment_20 Index',
-    'A': 'Asymmetry Index',
+    'M': 'log(Moment_20) Index',
+    'A': 'log(Asymmetry) Index',
     'C': 'Concentration Index',
 }
-draw(data, 'G', ar[0, 0], 'type', model='kde', func=lambda x: np.log10(x))
-draw(data, 'M', ar[0, 1], 'type', model='kde', func=lambda x: np.log10(x))
-draw(data, 'A', ar[1, 0], 'type', model='kde', func=lambda x: np.log10(x))
-draw(data, 'C', ar[1, 1], 'type', model='kde', func=lambda x: np.log10(x))
+# draw(data, 'G', ar[0, 0], 'type', model='kde')
+draw(data, 'M', ar[0, 1], 'type', model='kde')
+draw(data, 'A', ar[1, 0], 'type', model='kde')
+draw(data, 'C', ar[1, 1], 'type', model='kde')
 plt.show()
